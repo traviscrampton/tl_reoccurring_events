fc215218dff4486ce362f15f93295894272a3f0f
Travis Crampton <crampton.travis@gmail.com>
2017-10-05 11:03:21 -0700

    add validations to model for validates_presence_of and custom validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,26 @@
 class Event < ActiveRecord::Base
-	
+		validates_presence_of :label, :starting_date, :month_frequency, :day
+		validates_date :starting_date
+		validates :is_actual_frequency?
+		validates :is_month_day?
+
+
+		private
+
+		# this will validate that the month frequency is greater than 0
+		def is_actual_frequency?
+			if month_frequency < 1
+				errors.add(:month_frequency, " must be greater than 0")
+			end
+		end
+
+		#validates that the input will be between 1 and 31
+		def is_month_day?
+			if day > 31
+				errors.add(:day, " number cannot be greater than 31.")
+			elsif day < 1
+				errors.add(:day, " number cannot be less than one.")
+			end
+		end
+
 end
