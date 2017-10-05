3bc410b33e9166e130ff0bf61dc73d2000a7af8d
Travis Crampton <crampton.travis@gmail.com>
2017-10-05 11:23:27 -0700

    added day validation parameters to spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,5 +12,17 @@ RSpec.describe Event, :type => :model do
 			expect(event.errors[:month_frequency]).to include("can't be blank")
 			expect(event.errors[:day]).to include("can't be blank")
 		end
+
+		it 'validates that day is greater than 0' do
+			event = Event.new(day: 0)
+			event.valid?
+			expect(event.errors[:day]).to include(" number cannot be less than one.")
+		end
+
+		it 'validates that day is less than 31' do
+			event = Event.new(day: 35)
+			event.valid?
+			expect(event.errors[:day]).to include(" number cannot be greater than 31.")
+		end
 	end
 end
