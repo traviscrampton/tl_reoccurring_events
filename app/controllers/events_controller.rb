b8fa7613d8270de734f58fc67e91ae807d952f71
Travis Crampton <crampton.travis@gmail.com>
2017-10-05 11:56:44 -0700

    created form for a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,15 +4,26 @@ class EventsController < ApplicationController
 		@events = Event.all
 	end
 
+	def new
+		@event = Event.new
+	end
+
+	def create
+	end
+
 	def show
 	end
 
-	def new
-	end
 
 	def edit
 	end
 
 	def destroy
+	end
+
+	private
+
+	def event_params
+		params.require(:event).permit(:label, :starting_date, :month_frequency, :day)
 	end
 end
